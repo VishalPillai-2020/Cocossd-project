8999e9ec5e54fb8ee1f7d054acca9479dfb22142
Vishal Pillai <68294182+VishalPillai-2020@users.noreply.github.com>
2021-05-26 20:13:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function redirect1(){
+    window.location = "bedroom.html"
+}
+function redirect2(){
+    window.location = "computer.html"
+}
+function redirect3(){
+    window.location = "bottles.html"
+}
+function redirect4(){
+    window.location = "desk.html"
+}
